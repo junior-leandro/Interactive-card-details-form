7964c1ff6699b93b3ab9969a22f728a9e0d71049
Junior Oliveira <contato_jr@outlook.com>
2023-05-26 11:56:47 -0300

    Alguns bugs corrigidos e inicialização das funcionalidades de JavaScript para preenchimendo das informações

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
+const form = document.querySelector(".formulario");
+const formSection = document.querySelector(".form-section");
+const completed = document.querySelector(".finish-section");
+const buttonSend = document.querySelector("#confirm");
+const buttonContinue = document.querySelector(".finish-button");
 
-var form = document.querySelector("#formulario");
+let cardName = document.querySelector(".card-name");
+let cardNameInput = document.querySelector("#inputCardName");
+let cardNumber = document.querySelector(".card-number");
+let cardNumberInput = document.querySelector("#inputCardNumber");
+let cardDate =document.querySelector(".card-exp");
+let cardMonthInput = document.querySelector("#inputCardMounth");
+let cardYearInput = document.querySelector("#inputCardYear");
+let cardCvc = document.querySelector(".card-cvc");
+let cardCvcInput = document.querySelector("#inputCardCvc");
 
-const btn = document.querySelector("#confirm");
 
-btn.addEventListener("click", function(e){
+buttonSend.addEventListener("click", function (e) {
     e.preventDefault();
 
     let creditCardName = document.querySelector("#inputCardName");
@@ -26,5 +38,33 @@ btn.addEventListener("click", function(e){
     let cardCvc = creditCardCvc.value;
     console.log(cardCvc);
 
-    form.reset();
+    
+
+
+    completed.classList.remove("hidden");
+    formSection.classList.add("hidden");
 });
+
+
+
+buttonContinue.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.location.reload(true);
+
+});
+function setCardName(e) {
+    cardName.innerText = format(e.target.value);
+}
+function setCardNumber(e) {
+    cardNumber.innerText = format(e.target.value);
+}
+
+function format(s) {
+    return s.toString().replace(/\d{4}(?=.)/g, "$& ");
+  }
+
+
+  
+cardNameInput.addEventListener("keyup", setCardName);
+cardNumberInput.addEventListener("keyup", setCardNumber);
