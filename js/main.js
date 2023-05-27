dede44f579a6659411c0da2dbad0cf0eb2c9eb0c
Junior Oliveira <contato_jr@outlook.com>
2023-05-27 15:24:24 -0300

    Ajuste CSS - Padrão de escrita da Esquerda

--- js/main.js
+++ js/main.js
@@ -15,37 +15,6 @@ let cardCvc = document.querySelector(".card-cvc");
 let cardCvcInput = document.querySelector("#inputCardCvc");
 
 
-buttonSend.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    let creditCardName = document.querySelector("#inputCardName");
-    let cardName = creditCardName.value;
-    console.log(cardName);
-
-    let creditCardNumber = document.querySelector("#inputCardNumber");
-    let cardNumber = creditCardNumber.value;
-    console.log(cardNumber);
-
-    let creditCardMounth = document.querySelector("#inputCardMounth");
-    let cardMounth = creditCardMounth.value;
-    console.log(cardMounth);
-
-    let creditCardYear = document.querySelector("#inputCardYear");
-    let cardYear = creditCardYear.value;
-    console.log(cardYear);
-
-    let creditCardCvc = document.querySelector("#inputCardCvc");
-    let cardCvc = creditCardCvc.value;
-    console.log(cardCvc);
-
-    
-
-
-    completed.classList.remove("hidden");
-    formSection.classList.add("hidden");
-});
-
-
 
 buttonContinue.addEventListener("click", function (e) {
     e.preventDefault();
